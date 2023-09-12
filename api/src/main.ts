83ba454756d203f2b138bac79bb0176fb72632b4
Gergo Szucs <gergo_szucs@epam.com>
2023-09-12 14:40:53 +0200

    deploy testing

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,28 @@
 import express from 'express';
 import * as path from 'path';
+import fs from 'fs';
 
 const app = express();
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+console.log(process.env.NODE_ENV);
+console.log(__dirname);
+
+fs.readdir(__dirname, function (err, files) {
+  //handling error
+  if (err) {
+    return console.log('Unable to scan directory: ' + err);
+  }
+  //listing all files using forEach
+  files.forEach(function (file) {
+    // Do whatever you want to do with the file
+    console.log(file);
+  });
+});
 
 if (process.env.NODE_ENV === 'production') {
+  console.log('We are here');
+
   app.use('/', express.static('../apps/client'));
 }
 
